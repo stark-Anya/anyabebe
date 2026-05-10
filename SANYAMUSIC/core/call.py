2718d8f8afe3fdf6be8b26e35b557829d0901c7a
stark-Anya <misterxstark07@gmail.com>
2026-05-10 21:23:24 +0530

    Update and rename call (1).py to call.py

--- SANYAMUSIC/core/call.py
+++ SANYAMUSIC/core/call.py
@@ -1,16 +1,4 @@
-# -----------------------------------------------
-# 🔸 SanyaMusic Project
-# 🔹 Developed & Maintained by: Stark (https://github.com/urstark)
-# 📅 Copyright © 2022 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by urstark
-# -----------------------------------------------
+
 import asyncio
 import os
 from datetime import datetime, timedelta
